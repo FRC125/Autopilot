b371e41b369ae48cb2466bd3dd074de37579154d
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-02-14 16:11:00 -0500

    added activity to deal with creating trajectories

--- app/src/main/java/com/nutrons/autopilot/MainActivity.java
+++ app/src/main/java/com/nutrons/autopilot/MainActivity.java
@@ -1,13 +1,29 @@
 package com.nutrons.autopilot;
 
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton createPathActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.createPathActivity = (ImageButton) findViewById(R.id.createPathButton);
+
+        this.createPathActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreatePathActivity.class));
+            }
+        });
     }
 }

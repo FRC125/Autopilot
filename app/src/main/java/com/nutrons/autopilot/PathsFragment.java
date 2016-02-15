60d1c26d236ca2f267c3096461b1aef0e9e83de6
cappuccinocat <xinglydia@gmail.com>
2016-02-15 11:53:37 -0500

    Added ListView to PathsFragment but need to create child fragment

--- app/src/main/java/com/nutrons/autopilot/PathsFragment.java
+++ app/src/main/java/com/nutrons/autopilot/PathsFragment.java
@@ -1,23 +1,26 @@
 package com.nutrons.autopilot;
 
 
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PathsFragment extends Fragment {
-
-
+public class PathsFragment extends Fragment
+        //extends ListFragment implements AdapterView.OnItemClickListener
+    {
     public PathsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,4 +29,16 @@ public class PathsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_paths, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.PathNames, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Placeholder for click stuff" + position, Toast.LENGTH_SHORT).show();
+    }
 }

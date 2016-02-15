f0cb1ee9ce83b5c5cfb00472df5ba0dc0b75d70b
cappuccinocat <xinglydia@gmail.com>
2016-02-14 19:40:45 -0500

    Added navigation tabs

--- app/src/main/java/com/nutrons/autopilot/MainActivity.java
+++ app/src/main/java/com/nutrons/autopilot/MainActivity.java
@@ -1,14 +1,19 @@
 package com.nutrons.autopilot;
 
-import android.app.AlertDialog;
-import android.app.DialogFragment;
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     private ImageButton createPathActivity;
 
@@ -16,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new PathsFragment(), "Paths");
+        viewPagerAdapter.addFragments(new MatchesFragment(), "Matches");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
         this.createPathActivity = (ImageButton) findViewById(R.id.createPathButton);
 
